a0ad07e53743fcc46213b7331df47dca1fdc1ccf
miya <sirotumesirokuma@gmail.com>
2023-10-03 15:07:23 +0900

    main

--- api_project/asgi.py
+++ api_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for apiproject project.
+ASGI config for api_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- api_project/wsgi.py
+++ api_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for apiproject project.
+WSGI config for api_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
